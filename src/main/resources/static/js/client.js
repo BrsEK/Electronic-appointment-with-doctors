1384c4ddb3ceb59a469f436d5b726f3d5ac2dec4
Boris <krinitsky.B@yandex.ru>
2021-02-01 23:52:54 +0300

    + fillSelect

--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -1,5 +1,5 @@
+ // Запускаем функцию при изменении специализации
 $(function(){
-  // Запускаем функцию при изменении специализации
 src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
   $('#specSelect').on('change', function(){
     let spec_Id =  $(this).val();
@@ -9,14 +9,14 @@ src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
       dataType: 'json',
       url: '/client/doctors?specId=' + spec_Id
     }).done(function(doctors){
-      fillSelectDoctors(doctors); // выводит второй список по событию из первого селекта
+      fillSelect(doctors, "doctorSelect");
     })
 })
 })
 
 
+// Запускаем функцию при изменении врача
 $(function(){
-  // Запускаем функцию при изменении доктора
 src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
   $('#doctorSelect').on('change'  , function(){
     let doc_Id =  $(this).val();
@@ -26,7 +26,7 @@ src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
       dataType: 'json',
       url: '/client/dates?docId=' + doc_Id
     }).done(function(tickets){
-      fillSelectDate(tickets); // выводит третий список по событию из вторго селекта
+      fillSelect(tickets, "dateSelect");
     })
 })
 })
@@ -43,60 +43,35 @@ src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
        dataType: 'json',
        url: '/client/times?ticketId=' + ticketId
      }).done(function(listTicketsByDate){
-       fillSelectTime(listTicketsByDate);
+       fillSelect(listTicketsByDate, "timeSelect");
      })
 })
 })
 
 
-//Функция заполняет выпадающий список докторами
-function fillSelectDoctors(doctors){
-console.log("Method fillSelectDoctors");
-doctorsArr = doctors;
-var doctorSelect = document.getElementById("doctorSelect");
-doctorSelect.options.length = 1; // обнуляем options
-dateSelect.options.length = 1;
-timeSelect.options.length = 1;
-  for(var i = 0; i < doctorsArr.length; i++) {
-      var doctorFIO = Object(doctors[i].surname + ' ' + doctors[i].name + ' '  + doctors[i].patronymic);
-      var idDoctor = Object(doctors[i].id);
-      var el = document.createElement("option");
-      el.textContent = doctorFIO;
-      el.value = idDoctor;
-      doctorSelect.appendChild(el);
-      console.log(doctorFIO);
-  }
-}
-
-//Функция заполняет выпадающий список датами
-function fillSelectDate(tickets){
-console.log(ticketId);
-ticketsArr = tickets;
-var dateSelect = document.getElementById("dateSelect");
-dateSelect.options.length = 1; // обнуляем options
-timeSelect.options.length = 1;
-  for(var i = 0; i < ticketsArr.length; i++) {
-      var ticketId = Object(ticketsArr[i].id);
-      var date = Object(ticketsArr[i].date);
-      var el = document.createElement("option");
-      el.textContent = date;
-      el.value = ticketId;
-      dateSelect.appendChild(el);
-  }
-}
-
-
-//Функция заполняет выпадающий список временем
-function fillSelectTime(tickets){
-ticketsArr = tickets;
-var dateSelect = document.getElementById("timeSelect");
-dateSelect.options.length = 1; // обнуляем options
-  for(var i = 0; i < ticketsArr.length; i++) {
-      var ticketId = Object(ticketsArr[i].id);
-      var time = Object(ticketsArr[i].time);
-      var el = document.createElement("option");
-      el.textContent = time;
-      el.value = ticketId;
-      dateSelect.appendChild(el);
+//Функция заполняет выпадающий список
+function fillSelect(arr, nameSelect){
+var select = document.getElementById(nameSelect);
+select.options.length = 1;
+var textContent;
+var value;
+  for(var i = 0; i < arr.length; i++) {
+    if(nameSelect == "doctorSelect"){
+          textContent = arr[i].surname + ' ' + arr[i].name + ' '  + arr[i].patronymic;
+          value = arr[i].id;
+    }
+    if(nameSelect == "dateSelect"){
+            textContent = arr[i].date;
+            value = arr[i].id;
+            console.log(textContent);
+    }
+    if(nameSelect == "timeSelect"){
+            textContent = arr[i].time;
+            value = arr[i].id;
+    }
+     var el = document.createElement("option");
+      el.textContent = textContent;
+      el.value = value;
+      select.appendChild(el);
   }
 }
